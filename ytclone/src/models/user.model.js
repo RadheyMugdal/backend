067a193d47474438c9557311f48bf25c4fbcf28b
Radhey Mugdal <mugdalradhey@gmail.com>
2024-04-11 12:44:48 +0530

    final backend

--- ytclone/src/models/user.model.js
+++ ytclone/src/models/user.model.js
@@ -1,93 +1,71 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs'
+import mongoose from 'mongoose'
 
-const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true, 
-            index: true
+
+const userSchema=mongoose.Schema({
+        id:{
+           type:String,
+           required:true,
+           unique:true
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowecase: true,
-            trim: true, 
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
         },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true, 
-            index: true
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            
         },
-        avatar: {
-            type: String, // cloudinary url
-            required: true,
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
         },
-        coverImage: {
-            type: String, // cloudinary url
+        avatar:{
+            type:String,  // cloudinary
+            required:true
         },
-        watchHistory: [
+        coverImage:{
+            type:String   //cloudinary
+        },
+        watchHistory:[
             {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Video"
             }
         ],
-        password: {
-            type: String,
-            required: [true, 'Password is required']
+        password:{
+            type:String,
+            required:[true,"Password is required"]
         },
-        refreshToken: {
-            type: String
+        refreshToken:{
+            type:String,
         }
 
-    },
-    {
-        timestamps: true
+
+
+
+
+},{timestamps:true})
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")){
+        return next()
     }
-)
-
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
-
-    this.password = await bcrypt.hash(this.password, 10)
+    this.password =bcrypt.hash(this.password,10)
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect=async 
 
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName: this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
 
-export const User = mongoose.model("User", userSchema)
+const User=mongoose.model("User",userSchema)
--- ytclone/src/utils/asyncHandler.js
+++ ytclone/src/utils/asyncHandler.js
@@ -1,8 +1,24 @@
 
 const asyncHandler=(requestHandler)=>{
-    return (req,res,next)=>{
+    (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
 
-export { asyncHandler }
+
+
+
+// or 
+
+
+// const asyancHandler=(fn)=> async (req,res,next)  =>{
+//      try{
+//         await fn(req,res,next)
+//      }
+//      catch(err){
+//         res.status(err.code || 300).json({
+//             success:false,
+//             massage:err.message
+//         })
+//      }
+// };
